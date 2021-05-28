10e268cbe070a06561a3f1629788947bcc46c16b
Harinee <55607@hexaware.com>
2021-05-28 14:10:54 +0530

    Vendor & Admin Models

---
+++ OnlineClothStore/Models/Vendor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineClothStore.Models
+{
+    public class Vendor
+    {
+        public int VendorId { get; set; }
+        public string VendorName { get; set; }
+        public string VendorPassword { get; set; }
+        public string VendorEmail { get; set; }
+        public string VendorPhone { get; set; }
+        public string VendorAddress { get; set; }
+        public string VWalletBalance { get; set; }
+    }
+}
