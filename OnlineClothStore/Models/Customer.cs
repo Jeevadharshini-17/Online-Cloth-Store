6ff01a9b0ad17412202c12995709abc080940e99
Jeevadharshini <55460@hexaware.com>
2021-05-28 13:41:23 +0530

    Customer and Inventory Models

---
+++ OnlineClothStore/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineClothStore.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerEmail { get; set; }
+
+        public string CustomerPassword { get; set; }
+
+        public string CustomerAddress { get; set; }
+
+        public string CustomerPhone { get; set; }
+
+        public float CustomerWalletBalance { get; set; }
+    }
+}
---
+++ OnlineClothStore/Models/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineClothStore.Models
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int ProductQuantity { get; set; }
+
+        public float ProductPrice { get; set; }
+    }
+}
