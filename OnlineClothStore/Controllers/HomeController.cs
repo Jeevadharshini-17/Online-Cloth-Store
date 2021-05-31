6d0058d47ec21ad5bb9b0c50aaf86c2cb8bbd281
Indurthi <55611@hexaware.com>
2021-05-31 18:04:27 +0530

    Product search, completed

--- OnlineClothStore/Controllers/HomeController.cs
+++ OnlineClothStore/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using OnlineClothStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,41 @@ namespace OnlineClothStore.Controllers
 {
     public class HomeController : Controller
     {
+        private OnlineClothStoreDBContext db = new OnlineClothStoreDBContext();
         public ActionResult Index()
         {
-            return View();
+            //var vendorName = 
+            return View(db.Product.ToList());
+        }
+
+        // GET: Products/Details/5 (Search By vendor ID)
+        public ActionResult DetailsVendor(int? Vid)
+        {
+            if (Vid == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var product = db.Product.Where(p => p.VendorId == Vid);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product.ToList());
+        }
+
+        // GET: Products/Details/5 ( Search By Name)
+        public ActionResult DetailsName(string name)
+        {
+            if (name == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var product = db.Product.Where(p => p.ProductName.Contains(name));
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product.ToList());
         }
 
         public ActionResult About()
